d43da3a6111714d6d47bcabca3b45003f05a431e
Shootingmemory <yhz2389441063@163.com>
2025-01-24 22:12:46 +0800

    0124

---
+++ J20250124/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+     public class ListNode {
+     int val;
+        ListNode next = null;
+        public ListNode(int val) {
+         this.val = val;
+      }
+     }
+
+    public class Solution {
+        public ListNode Merge(ListNode list1, ListNode list2) {
+            if (list1 == null) {
+                return list2;
+            } else if (list2 == null) {
+                return list1;
+            }
+            if (list2.val > list1.val) {
+                list1.next = Merge(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = Merge(list1, list2.next);
+                return list2;
+            }
+        }
+    }
+}
