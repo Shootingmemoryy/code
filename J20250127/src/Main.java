c29ae62c6369643624450268e3637c0b01a1a868
Shootingmemory <yhz2389441063@163.com>
2025-01-27 21:33:57 +0800

    0127

---
+++ J20250127/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+
+      public class ListNode {
+        int val;
+        ListNode next = null;
+      }
+    public ListNode deleteDuplicates (ListNode head) {
+        if(head == null) return null;
+        ListNode fast = head;
+        while(fast.next != null) {
+            if (fast.val == fast.next.val) {
+                fast.next = fast.next.next;
+            } else {
+                fast = fast.next;
+            }
+        }
+        return head;
+    }
+}
