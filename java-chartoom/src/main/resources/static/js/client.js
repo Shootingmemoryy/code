5d8027ac4238aae71d81f1feff1bb00ff0aefc11
Shootingmemory <yhz2389441063@163.com>
2025-02-05 23:01:03 +0800

    0205

--- java-chartoom/src/main/resources/static/js/client.js
+++ java-chartoom/src/main/resources/static/js/client.js
@@ -1,3 +1,19 @@
 function initSwitchTab() {
-    
+    let tabSession = document.querySelector('.tab .tab-session');
+    let tabFriend =document.querySelector('.tab .tab-friend');
+    let lists =document.querySelectorAll('.list');
+    tabSession.onclick = function () {
+        tabSession.style.backgroundImage = "url('img/Chat.png')";
+        tabFriend.style.backgroundImage = "url('img/User1.png')";
+        lists[0].classList= 'list';
+        lists[1].classList='list hide';
+
+    }
+    tabFriend.onclick = function () {
+        tabSession.style.backgroundImage = "url('img/Chat1.png')";
+        tabFriend.style.backgroundImage = "url('img/User.png')";
+        lists[0].classList='list hide';
+        lists[1].classList='list';
+    }
 }
+initSwitchTab();
