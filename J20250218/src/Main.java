3d337acf0b119d4bea16a58efcda66518577092d
Shootingmemory <yhz2389441063@163.com>
2025-02-18 14:16:37 +0800

    0218

---
+++ J20250218/src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * @Shootingmemory
+ * @create 2025-02-18-13:57
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+      class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if(head == null || head.next == null){
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while(slow != fast){
+                if(fast==null||fast.next==null){
+                    return false;
+                }
+
+                slow=slow.next;
+                fast=fast.next.next;
+            }
+            return true;
+        }
+    }
+}
