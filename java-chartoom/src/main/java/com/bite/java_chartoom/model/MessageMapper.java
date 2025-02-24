d647efb9680dc2a6df87e3a857c3b7840c461734
Shootingmemory <yhz2389441063@163.com>
2025-02-24 21:25:51 +0800

    0224

--- java-chartoom/src/main/java/com/bite/java_chartoom/model/MessageMapper.java
+++ java-chartoom/src/main/java/com/bite/java_chartoom/model/MessageMapper.java
@@ -2,6 +2,8 @@ package com.bite.java_chartoom.model;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @Shootingmemory
  * @create 2025-02-21-15:10
@@ -9,4 +11,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MessageMapper {
     String getLastMessageBySessionId(int sessionId);
+
+    List<Message> getMessagesBySessionId(int sessionId);
 }
