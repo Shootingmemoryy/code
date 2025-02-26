6444b29d2676f0a597cafdb461363817e0a319aa
Shootingmemory <yhz2389441063@163.com>
2025-02-26 22:24:28 +0800

    0226

--- J20250225/src/Main.java
+++ J20250225/src/Main.java
@@ -1,4 +1,5 @@
- /**
+import java.util.ArrayList;
+/**
     @Shootingmemory
     @create 2025-02-25-19:19
     
@@ -6,6 +7,30 @@
 
 public class Main {
     public static void main(String[] args) {
-
     }
+
+     public class Solution {
+         public void recursion(int left, int right, String temp, ArrayList<String> res, int n){
+             //左右括号都用完了，就加入结果
+             if(left == n && right == n){
+                 res.add(temp);
+                 return;
+             }
+             //使用一次左括号
+             if(left < n){
+                 recursion(left + 1, right, temp + "(", res, n);
+             }
+             //使用右括号个数必须少于左括号
+             if(right < n && left > right){
+                 recursion(left, right + 1, temp + ")", res, n);
+             }
+         }
+         public ArrayList<String> generateParenthesis (int n) {
+             //记录结果
+             ArrayList<String> res = new ArrayList<String>();
+             //递归
+             recursion(0, 0, "", res, n);
+             return res;
+         }
+     }
 }
