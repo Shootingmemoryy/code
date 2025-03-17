7ce2c2bf36e9b87d21fb4d394b5d06d692b8934c
Shootingmemory <yhz2389441063@163.com>
2025-03-17 15:03:14 +0800

    20250317

---
+++ J202503141/src/main/java/com/bite/j202503141/J202503141Application.java
@@ -0,0 +1,13 @@
+package com.bite.j202503141;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J202503141Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J202503141Application.class, args);
+    }
+
+}
---
+++ J202503141/src/test/java/com/bite/j202503141/J202503141ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bite.j202503141;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J202503141ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
