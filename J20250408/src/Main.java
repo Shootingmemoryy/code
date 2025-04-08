26937ae5945012c35eacaacdb435b4523f75ec9a
Shootingmemory <yhz2389441063@163.com>
2025-04-08 22:48:53 +0800

    0408

---
+++ J20250408/src/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * @Shootingmemory
+ * @create 2025-04-08-22:47
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+}
