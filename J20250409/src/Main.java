b8e787b143d9c1e7e08be891f744c478b4dc1b55
Shootingmemory <yhz2389441063@163.com>
2025-04-09 20:18:33 +0800

    0409

---
+++ J20250409/src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @Shootingmemory
+ * @create 2025-04-09-20:16
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode newHead = head.next;
+            head.next = swapPairs(newHead.next);
+            newHead.next = head;
+            return newHead;
+        }
+    }
+
+}
