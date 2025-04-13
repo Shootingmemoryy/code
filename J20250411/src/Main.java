0673567b83fccbfa913bef88eac416f8d22b9ade
Shootingmemory <yhz2389441063@163.com>
2025-04-13 23:29:23 +0800

    0413

---
+++ J20250411/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @Shootingmemory
+ * @create 2025-04-11-19:06
+ */
+public class Main {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                String order1 = s1 + s2;
+                String order2 = s2 + s1;
+                return order2.compareTo(order1);
+            }
+        });
+        if (strs[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
