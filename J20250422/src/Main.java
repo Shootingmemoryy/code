026271df8f4a84fc736ae9061193fc762983fe3f
Shootingmemory <yhz2389441063@163.com>
2025-04-22 14:12:53 +0800

    0422

---
+++ J20250422/src/Main.java
@@ -0,0 +1,29 @@
+/**
+ * @Shootingmemory
+ * @create 2025-04-22-13:17
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+ /*你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。*/
+    class Solution {
+        public int rob(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int length = nums.length;
+            if (length == 1) {
+                return nums[0];
+            }
+            int[] dp = new int[length];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < length; i++) {
+                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+            return dp[length - 1];
+        }
+    }
+
+
+}
